3f19130e0570c04ceeebaad20977bb43b4f1c5eb
Jessie Chen <notionj.jc@gmail.com>
2023-03-25 16:29:50 -0400

    Add extra education to preview and change education preview  CSS

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/Preview.css';
 import ExtraExperiencePreview from './extraExperiencePreview';
+import ExtraEducationPreview from './extraEducationPreview';
 
 
 class Preview extends Component {
@@ -9,6 +10,14 @@ class Preview extends Component {
         const expChildren = [];
         for (let i = 1; i <= this.props.data.other.numChildrenExp; i += 1) {
             expChildren.push(<ExtraExperiencePreview
+                i={i}
+                data={this.props.data}
+            />)
+        }
+
+        const eduChildren = [];
+        for (let i = 1; i <= this.props.data.other.numChildrenEdu; i += 1) {
+            eduChildren.push(<ExtraEducationPreview
                 i={i}
                 data={this.props.data}
             />)
@@ -43,14 +52,17 @@ class Preview extends Component {
                     <p className="previewSectionTitle">
                         Education
                     </p>
-                    <div>
-                        <p>{this.props.data.education.university}</p>
-                        <p>{this.props.data.education.eduCity}</p>
-                        <p>{this.props.data.education.eduStart}</p>
-                        <p>{this.props.data.education.eduEnd}</p>
-                        <p>{this.props.data.education.degree}</p>
-                        <p>{this.props.data.education.subject}</p>
+                    <div className='previewGroup'>
+                        <span>{this.props.data.education.university}{', '}</span>
+                        <span>{this.props.data.education.eduCity}{' '}</span>
+                        <span>{'('}{this.props.data.education.eduStart}{' to '}</span>
+                        <span>{this.props.data.education.eduEnd}{')'}</span>
+                        <p>
+                            <span>{this.props.data.education.degree}{' in '}</span>
+                            <span>{this.props.data.education.subject}</span>
+                        </p>
                     </div>
+                    {eduChildren}
                 </div>
             </div>
         );
--- src/components/extraEducationPreview.js
+++ src/components/extraEducationPreview.js
@@ -6,12 +6,14 @@ class ExtraEducationPreview extends Component {
         let x = 'edu'+this.props.i;
         return(
             <div className='previewGroup'>
-                <p>{this.props.data[x].university}{', '}</p>
+                <span>{this.props.data[x].university}{', '}</span>
                 <span>{this.props.data[x].eduCity}{' '}</span>
                 <span>{'('}{this.props.data[x].eduStart}{' to '}</span>
                 <span>{this.props.data[x].eduEnd}{')'}</span>
-                <p>{this.props.data[x].degree}{' in '}</p>
-                <span>{this.props.data[x].subject}</span>
+                <p>
+                    <span>{this.props.data[x].degree}{' in '}</span>
+                    <span>{this.props.data[x].subject}</span>
+                </p>
             </div>
         )
     }
