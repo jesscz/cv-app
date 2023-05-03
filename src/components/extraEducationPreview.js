944930b712dcc8e1cab8b9eef9c00bcece18ebdb
jesscz <notionj.jc@gmail.com>
2023-05-03 17:06:04 -0400

    Convert extraEducationPreview.js from a class component to a functional component

--- src/components/extraEducationPreview.js
+++ src/components/extraEducationPreview.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from "react";
 
-
-class ExtraEducationPreview extends Component {
-    render(){
-        let x = 'edu'+this.props.i;
-        return(
-            <div className='previewGroup'>
-                <span>{this.props.data[x].university}{', '}</span>
-                <span>{this.props.data[x].eduCity}{' '}</span>
-                <span>{'('}{this.props.data[x].eduStart}{' to '}</span>
-                <span>{this.props.data[x].eduEnd}{')'}</span>
-                <p>
-                    <span>{this.props.data[x].degree}{' in '}</span>
-                    <span>{this.props.data[x].subject}</span>
-                </p>
-            </div>
-        )
-    }
+function ExtraEducationPreview({ i, data }) {
+ let x = 'edu'+i;
+ return(
+    <div className='previewGroup'>
+        <span>{data[x].university}{', '}</span>
+        <span>{data[x].eduCity}{' '}</span>
+        <span>{'('}{data[x].eduStart}{' to '}</span>
+        <span>{data[x].eduEnd}{')'}</span>
+        <p>
+            <span>{data[x].degree}{' in '}</span>
+            <span>{data[x].subject}</span>
+        </p>
+    </div>
+ )
 }
 
 export default ExtraEducationPreview;
