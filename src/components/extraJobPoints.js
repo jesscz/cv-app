2522d2e76464bf430572f7bb055a0aa16b3f20d2
jesscz <notionj.jc@gmail.com>
2023-05-03 16:26:26 -0400

    Convert extraJobPoints.js from a class component to a functional component

--- src/components/extraJobPoints.js
+++ src/components/extraJobPoints.js
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ExtraJobPoints extends Component {
-    render() {
-        return (
-            <input
-                type='text'
-                placeholder='Job point'
-                className={this.props.i}
-                onChange={this.props.onDescriptionChange}
-            />
-        )
-    }
+function ExtraJobPoints(props) {
+    const { i, onDescriptionChange } = props;
+ return (
+    <input 
+        type="text"
+        placeholder="Job Point"
+        className={i}
+        onChange={onDescriptionChange}
+    />
+ );
 }
 
 export default ExtraJobPoints;
