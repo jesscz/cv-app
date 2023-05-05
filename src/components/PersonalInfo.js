45e2563ff614b3447c35a84e4cd7a7a36afee5bc
jesscz <notionj.jc@gmail.com>
2023-05-05 17:47:02 -0400

    Convert PersonalInfo.js from a class component to a functional component

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,65 +1,62 @@
-import React, { Component } from 'react';
-// import DisplayImage from './DisplayImage';
+import React from 'react';
 
-class PersonalInfo extends Component {
-    render() {
-        return (
-            <form>
-                <p
-                    id="personalInfo" 
-                    className="title">
-                    Personal Information:
-                </p>
-                <label htmlFor="firstName">
-                    <input 
-                        onChange= {this.props.onChange}
-                        type="text" 
-                        placeholder="First name"
-                        id="firstName" 
-                    />
-                </label>
-                <label htmlFor="lastName">
-                    <input 
-                        onChange= {this.props.onChange}
-                        type="text" 
-                        placeholder="Last name"
-                        id="lastName" 
-                    />
-                </label>
-                <label htmlFor="photoId">Select photo ID image: 
-                    <input 
-                        onChange= {this.props.onPhotoChange}
-                        type="file" 
-                        id="photoId" 
-                    />
-                </label>
-                <label htmlFor="email">
-                    <input 
-                        onChange= {this.props.onChange}
-                        type="text" 
-                        placeholder="Email"
-                        id="email" 
-                    />
-                </label>
-                <label htmlFor="phoneNumber">
-                    <input 
-                        onChange= {this.props.onChange}
-                        type="text" 
-                        placeholder="Phone number"
-                        id="phoneNumber" 
-                    />
-                </label>
-                <label htmlFor="address">
-                    <input 
-                        onChange= {this.props.onChange}
-                        type="text" 
-                        placeholder="Address"
-                        id="address" 
-                    />
-                </label>
-            </form>
-        );
-    }
+function PersonalInfo({ onChange, onPhotoChange }) {
+ return (
+    <form>
+        <p
+            id="personalInfo" 
+            className="title">
+            Personal Information:
+        </p>
+        <label htmlFor="firstName">
+            <input 
+                onChange= {onChange}
+                type="text" 
+                placeholder="First name"
+                id="firstName" 
+            />
+        </label>
+        <label htmlFor="lastName">
+            <input 
+                onChange= {onChange}
+                type="text" 
+                placeholder="Last name"
+                id="lastName" 
+            />
+        </label>
+        <label htmlFor="photoId">Select photo ID image: 
+            <input 
+                onChange= {onPhotoChange}
+                type="file" 
+                id="photoId" 
+            />
+        </label>
+        <label htmlFor="email">
+            <input 
+                onChange= {onChange}
+                type="text" 
+                placeholder="Email"
+                id="email" 
+            />
+        </label>
+        <label htmlFor="phoneNumber">
+            <input 
+                onChange= {onChange}
+                type="text" 
+                placeholder="Phone number"
+                id="phoneNumber" 
+            />
+        </label>
+        <label htmlFor="address">
+            <input 
+                onChange= {onChange}
+                type="text" 
+                placeholder="Address"
+                id="address" 
+            />
+        </label>
+    </form>
+ );
 }
 
 export default PersonalInfo;
