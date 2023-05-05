028a9f7af0fa3e9ed5f5c6ae917eaa93e43b4332
jesscz <notionj.jc@gmail.com>
2023-05-05 19:18:07 -0400

    Convert Preview.js from a class component to a functional component

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,80 +1,78 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/Preview.css';
 import ExtraExperiencePreview from './extraExperiencePreview';
 import ExtraEducationPreview from './extraEducationPreview';
 
 
-class Preview extends Component {
-    render() {
+function Preview({ data }) {
 
-        const expChildren = [];
-        for (let i = 1; i <= this.props.data.other.numChildrenExp; i += 1) {
-            expChildren.push(<ExtraExperiencePreview
-                i={i}
-                data={this.props.data}
-            />)
-        }
+ const expChildren = [];
+ for (let i = 1; i <= data.other.numChildrenExp; i += 1) {
+    expChildren.push(<ExtraExperiencePreview
+        i={i}
+        data={data}
+    />)
+ }
 
-        const eduChildren = [];
-        for (let i = 1; i <= this.props.data.other.numChildrenEdu; i += 1) {
-            eduChildren.push(<ExtraEducationPreview
-                i={i}
-                data={this.props.data}
-            />)
-        }
+ const eduChildren = [];
+ for (let i = 1; i <= data.other.numChildrenEdu; i += 1) {
+    eduChildren.push(<ExtraEducationPreview
+        i={i}
+        data={data}
+    />)
+ }
 
 
-        return (
-            <div className='preview'>
-                <div className='previewPersonalInfo'>
-                    <h1>{this.props.data.personalInfo.firstName} {this.props.data.personalInfo.lastName}</h1>
-                    <img src={this.props.data.personalInfo.photoId} alt='user ID'/>
-                    <p id='personalInfoL2'>
-                        <span>{this.props.data.personalInfo.email}{' | '}</span>
-                        <span>{this.props.data.personalInfo.phoneNumber}{' | '}</span>
-                        <span>{this.props.data.personalInfo.address}</span>
-                    </p>
-                </div>
-                <div>
-                    <p className="previewSectionTitle">   
-                        Experience
-                    </p>
-                    <div className='previewGroup'>
-                        <p className='previewCompany'>{this.props.data.experience.company}</p>
-                        <span>{this.props.data.experience.position}{' '}</span>
-                        <span className='expCity'>{this.props.data.experience.expCity}{' '}</span>
-                        <span>{'('}{this.props.data.experience.expStart}{' to '}</span>
-                        <span>{this.props.data.experience.expEnd}{')'}</span>
-                        <ul>{this.props.data.experience.description
-                            .filter((i) => {
-                                return i !== '';
-                            })
-                            .map((i) => {
-                                return <li>{[i]}</li>
-                            })
-                        }</ul>
-                    </div>
-                    {expChildren}
-                </div>
-                <div>
-                    <p className="previewSectionTitle">
-                        Education
-                    </p>
-                    <div className='previewGroup'>
-                        <span>{this.props.data.education.university}{', '}</span>
-                        <span>{this.props.data.education.eduCity}{' '}</span>
-                        <span>{'('}{this.props.data.education.eduStart}{' to '}</span>
-                        <span>{this.props.data.education.eduEnd}{')'}</span>
-                        <p>
-                            <span>{this.props.data.education.degree}{' in '}</span>
-                            <span>{this.props.data.education.subject}</span>
-                        </p>
-                    </div>
-                    {eduChildren}
-                </div>
+ return (
+    <div className='preview'>
+        <div className='previewPersonalInfo'>
+            <h1>{data.personalInfo.firstName} {data.personalInfo.lastName}</h1>
+            <img src={data.personalInfo.photoId} alt='user ID'/>
+            <p id='personalInfoL2'>
+                <span>{data.personalInfo.email}{' | '}</span>
+                <span>{data.personalInfo.phoneNumber}{' | '}</span>
+                <span>{data.personalInfo.address}</span>
+            </p>
+        </div>
+        <div>
+            <p className="previewSectionTitle">   
+                Experience
+            </p>
+            <div className='previewGroup'>
+                <p className='previewCompany'>{data.experience.company}</p>
+                <span>{data.experience.position}{' '}</span>
+                <span className='expCity'>{data.experience.expCity}{' '}</span>
+                <span>{'('}{data.experience.expStart}{' to '}</span>
+                <span>{data.experience.expEnd}{')'}</span>
+                <ul>{data.experience.description
+                    .filter((i) => {
+                        return i !== '';
+                    })
+                    .map((i) => {
+                        return <li>{[i]}</li>
+                    })
+                }</ul>
             </div>
-        );
-    }
+            {expChildren}
+        </div>
+        <div>
+            <p className="previewSectionTitle">
+                Education
+            </p>
+            <div className='previewGroup'>
+                <span>{data.education.university}{', '}</span>
+                <span>{data.education.eduCity}{' '}</span>
+                <span>{'('}{data.education.eduStart}{' to '}</span>
+                <span>{data.education.eduEnd}{')'}</span>
+                <p>
+                    <span>{data.education.degree}{' in '}</span>
+                    <span>{data.education.subject}</span>
+                </p>
+            </div>
+            {eduChildren}
+        </div>
+    </div>
+ );
 }
 
 export default Preview;
